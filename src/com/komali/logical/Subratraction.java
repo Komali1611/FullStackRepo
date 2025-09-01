04763ce2565e71d854bbd53a1e2ec72e9c323929
Vinaykumar <Vinaykumar@DESKTOP-RFND8HI>
2025-09-01 11:00:34 +0530

    Subtraction Of Two Numbers

---
+++ src/com/komali/logical/Subratraction.java
@@ -0,0 +1,11 @@
+package com.komali.logical;
+
+public class Subratraction {
+
+	public static void main(String[] args) {
+		
+		int a =10, b =20;
+		int sub = a-b;
+		System.out.println("Subtraction :"+sub);
+	}
+}
